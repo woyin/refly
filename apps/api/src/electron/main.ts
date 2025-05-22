60e979b657d4e36755f6d348cf5fe7317e88c550
mrcfps <mrc@powerformer.com>
2025-05-22 21:01:57 +0800

    chore(electron): make packaging process successful

--- apps/api/src/electron/main.ts
+++ apps/api/src/electron/main.ts
@@ -10,11 +10,9 @@ import { shutdownApiServer, startApiServer } from './services/api';
 process.env.APP_ROOT = path.join(__dirname, '..');
 
 export const VITE_DEV_SERVER_URL = 'http://localhost:5173';
-export const RENDERER_DIST = path.join(process.env.APP_ROOT, 'dist');
+export const RENDERER_DIST = path.join(process.env.APP_ROOT, 'renderer');
 
-process.env.VITE_PUBLIC = VITE_DEV_SERVER_URL
-  ? path.join(process.env.APP_ROOT, 'public')
-  : RENDERER_DIST;
+process.env.VITE_PUBLIC = RENDERER_DIST || path.join(process.env.APP_ROOT, 'public');
 
 let win: BrowserWindow | null;
 
