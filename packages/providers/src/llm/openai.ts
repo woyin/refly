afe265091e8df9dcfbcff02c12480181ff317b88
mrcfps <mrc@powerformer.com>
2025-08-12 02:31:17 +0800

    refactor(provider): remove unused fields EnhanceChatOpenAI
    - Removed unnecessary eslint-disable comments for explicit any type usage.
    - Simplified the assignment of reasoning_content in the _convertCompletionsMessageToBaseMessage method.
    - Cleaned up the EnhancedChatOpenAI class by removing unused static methods and properties, improving code clarity and maintainability.

--- packages/providers/src/llm/openai.ts
+++ packages/providers/src/llm/openai.ts
@@ -8,7 +8,6 @@ import {
 
 class EnhanceChatOpenAICompletions extends ChatOpenAICompletions {
   protected override _convertCompletionsDeltaToBaseMessageChunk(
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     delta: Record<string, any>,
     rawResponse: OpenAIClient.ChatCompletionChunk,
     defaultRole?: 'function' | 'user' | 'system' | 'developer' | 'assistant' | 'tool',
@@ -27,26 +26,12 @@ class EnhanceChatOpenAICompletions extends ChatOpenAICompletions {
     rawResponse: OpenAIClient.ChatCompletion,
   ) {
     const langChainMessage = super._convertCompletionsMessageToBaseMessage(message, rawResponse);
-    langChainMessage.additional_kwargs.reasoning_content =
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      (message as any).reasoning_content;
+    langChainMessage.additional_kwargs.reasoning_content = (message as any).reasoning_content;
     return langChainMessage;
   }
 }
 
 export class EnhancedChatOpenAI extends ChatOpenAI<ChatOpenAICallOptions> {
-  static lc_name() {
-    return 'ChatOpenAI';
-  }
-
-  _llmType() {
-    return 'openai';
-  }
-
-  lc_serializable = true;
-
-  lc_namespace = ['langchain', 'chat_models', 'deepseek'];
-
   constructor(fields?: Partial<ChatOpenAIFields>) {
     super({
       ...fields,
