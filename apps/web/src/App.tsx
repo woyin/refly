aaeb1b3c6fcb65ff3cbb1e75567afa61ce34f189
xnmeet <xxnmeet@gmail.com>
2025-09-18 13:31:03 +0800

    chore: remove debug code

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,28 +1,24 @@
-import { useState } from 'react';
+import { Suspense } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { LightLoading } from '@refly/ui-kit';
 import { ErrorBoundary } from '@sentry/react';
 import { AppLayout } from '@refly/web-core';
 
+import { RoutesList } from './routes';
 import { InitializationSuspense } from './prepare/InitializationSuspense';
-import { Markdown } from '@refly/ai-workspace-common/components';
-import { Input } from 'antd';
 
 export const App = () => {
-  const [content, setContent] = useState('');
   return (
     <ErrorBoundary>
       <InitializationSuspense>
         <AppLayout>
-          <Input.TextArea onChange={(e) => setContent(e.target.value)} />
-          <div className="w-[800px] h-[800px] overflow-auto">
-            <Markdown content={content} />
-          </div>
-          {/* <Suspense fallback={<LightLoading />}>
+          <Suspense fallback={<LightLoading />}>
             <Routes>
               {RoutesList.map((route) => (
                 <Route key={route.path} path={route.path} element={route.element} />
               ))}
             </Routes>
-          </Suspense> */}
+          </Suspense>
         </AppLayout>
       </InitializationSuspense>
     </ErrorBoundary>
